3f8f74c90bf7151e65a865789ff2a71beb3880de
Matheus <matheusosp>
2022-10-13 12:08:54 -0300

    Adicionando banco de dados em Memoria

--- payment-api/Program.cs
+++ payment-api/Program.cs
@@ -6,12 +6,16 @@ using PaymentAPI.Domain.Interfaces;
 using PaymentAPI.Infra.EF;
 using PaymentAPI.Infra.EF.Context;
 using PaymentAPI.Infra.EF.Repositories;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+  (options) => options.UseInMemoryDatabase("PaymentAPI"));
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
