d10d7addf5b1a8f2f5576f1f78f6f0eb0ad8e490
Matheus <matheusosp>
2022-10-14 20:31:04 -0300

    Adicionando Variavel para determinar uso de banco de dados em memoria e ajustando Injeção de dependencia do MediatR

--- payment-api/Program.cs
+++ payment-api/Program.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using PaymentAPI.Application.Commands;
 using PaymentAPI.Application.Mapping;
+using PaymentAPI.Application.Queries;
 using PaymentAPI.Domain.Features;
 using PaymentAPI.Domain.Interfaces;
 using PaymentAPI.Infra.EF;
@@ -13,16 +14,24 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+if (bool.Parse(builder.Configuration["UseInMemory"]))
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(
+      (options) => options.UseInMemoryDatabase("PaymentAPI"));
+}
+else 
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+}
 
-//builder.Services.AddDbContext<ApplicationDbContext>(
-//  (options) => options.UseInMemoryDatabase("PaymentAPI"));
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ISaleRepository, SaleRepository>();
-builder.Services.AddMediatR(typeof(RegisterSaleCommand));
+builder.Services.AddMediatR(typeof(RegisterSaleCommandHandler));
+builder.Services.AddMediatR(typeof(UpdateSaleCommandHandler));
+builder.Services.AddMediatR(typeof(RetrieveSaleByIdQueryHandler));
 builder.Services.AddAutoMapper(typeof(AutoMapperInitializer));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
